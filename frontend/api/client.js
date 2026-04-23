1fdfb98cb30e752edbbbf94094e647462e0c88cc
tung2k40 <thanhtung26042004@.com>
2026-04-23 11:09:49 +0700

    fix base url

--- frontend/api/client.js
+++ frontend/api/client.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:8000";
+const BASE_URL = "https://book-review-backend-tau.onrender.com"; // Thay đổi bằng link Render CỦA BẠN
 
 /**
  * Base fetch wrapper for the application
